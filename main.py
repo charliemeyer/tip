5b887dc5210fb94cb7f55d52d6bcb245333ab677
Eric Bailey <popcorncolonel@gmail.com>
2015-10-24 04:58:07 -0400

    sound plaaaayyyyysssssssss

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 import json
 import jinja2
 import webapp2
+import urllib
+import urllib2
+import logging
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -14,8 +17,25 @@ class Question(object):
         self.question_text = question_text
         self.test_cases = test_cases
 
+class Test(webapp2.RequestHandler):
+    def post(self):
+        self.response.headers["Access-Control-Allow-Origin"] = "*"
+        self.response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+        self.response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT"
+        self.response.headers['Content-Type'] = 'application/json'   
+        url = "http://api.hackerrank.com/checker/submission.json"
+        logging.warning(self.request.params)
+        data = self.request.params
+        content = urllib2.urlopen(url=url, data=urllib.urlencode(data)).read()
+        logging.warning(content)
+        self.response.out.write(content)
+        
+
 class Questions(webapp2.RequestHandler):
     def get(self):
+        self.response.headers["Access-Control-Allow-Origin"] = "*"
+        self.response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+        self.response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT"
         self.response.headers['Content-Type'] = 'application/json'   
         questions = ["How fast is sorting?", "How slow is sorting?", "How slow are we at coding?"]
         self.response.out.write(json.dumps(questions))
@@ -34,7 +54,9 @@ class MainHandler(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
+    ('/test', Test),
     ('/questions.json', Questions),
     ('/interview', Interview),
     ('/', MainHandler)
 ], debug=True)
+
