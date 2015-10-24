9fa34b61b88b69ae53b114d3452439673797a816
Colin B Hamilton <cbhsixtysix@gmail.com>
2015-10-23 22:53:40 -0400

    Add chat history with some test messages.  Also add lodash.

--- js/ChatBox.js
+++ js/ChatBox.js
@@ -3,13 +3,22 @@ define([
         "dijit/_WidgetBase",
         "dojo/_base/fx",
         "dojo/query",
-        "dojo/dom-construct"
+        "dojo/dom-construct",
+        "dojo/dom-geometry",
+        "dojo/dom-style",
+        "dojo/on",
+
+        "js/lodash"
     ], function (
         declare,
         _WidgetBase,
         fx,
         query,
-        domConstruct
+        domConstruct,
+        domGeom,
+        domStyle,
+        on,
+        _
     ) {
 
     var ChatBox = declare([_WidgetBase], {
@@ -26,11 +35,36 @@ define([
         addMessage: function (messageText, timeOut) {
             timeOut = timeOut || this.defaultTimeOut;
             var messageBox = domConstruct.create("div", {
-                innerHTML: messageText
+                "innerHTML": messageText,
+                "class": "chat-message"
             }, this.domNode);
+            var messageHeight = domGeom.getMarginBox(messageBox).h;
+            console.log(messageHeight);
             setTimeout(function () {
-                fx.fadeOut({ node: messageBox, duration: 1000 }).play();
+                var fadeOutAnimation = fx.fadeOut({
+                        node: messageBox,
+                        duration: 1000
+                    });
+                on(fadeOutAnimation, "End", function () {
+                    //domConstruct.destroy(messageBox);
+                });
+                fadeOutAnimation.play();
             }, timeOut);
+            this._shiftUp(messageHeight, messageBox);
+        },
+
+        _shiftUp: function (amt, ignore) {
+            if (!_.isArray(ignore)) {
+                ignore = [ignore];
+            }
+            _.each(this.domNode.children, function (child) {
+                if (!_.includes(ignore, child)) {
+                    var bottom = parseInt(domStyle.get(child, "bottom"));
+                    domStyle.set(child, "bottom", bottom + amt + "px");
+                    console.log(child);
+                    console.log(bottom, amt)
+                }
+            });
         }
     });
 
